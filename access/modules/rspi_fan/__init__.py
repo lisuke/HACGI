6a2e337878711f67d26d2eec7562ab8e48bf921e
lisuke <1657787678@qq.com>
2018-04-27 19:15:47 +0800

    fix default status

---
+++ access/modules/rspi_fan/__init__.py
@@ -0,0 +1,1 @@
+from .service_instance import ServiceInstance
---
+++ access/modules/rspi_fan/service_instance.py
@@ -0,0 +1,44 @@
+from access.dbus.module_service_interface import ServiceABC
+import os
+
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(35, GPIO.OUT)
+
+
+def on(iface):
+    iface.is_on = True
+    print("press on")
+    GPIO.output(35, 1)
+
+
+def off(iface):
+    iface.is_on = False
+    print("press off")
+    GPIO.output(35, 0)
+
+
+def valueChanged(iface, value):
+    print("valueChanged, current value is {}.".format(value))
+
+
+class ServiceInstance(ServiceABC):
+
+    @property
+    def config_file_path(self):
+        return os.path.split(os.path.realpath(__file__))[0] + '/service.conf'
+
+    @property
+    def config_section(self):
+        return "CurrentConfig"
+
+    # #############################
+
+    def Init(self):
+        super(ServiceInstance, self).Init()
+        self.RegisterMethodToInterface(
+            "switch_interface", "on", on)
+        self.RegisterMethodToInterface(
+            "switch_interface", "off", off)
+        self.RegisterMethodToInterface(
+            "range_selector", "valueChanged", valueChanged)
